c2f95c4b76aeecba44c5874f663ce8468550c1d9
mateusns12 <mateusgms12@gmail.com>
2022-04-25 00:27:37 -0300

    main fd

--- src/main.c
+++ src/main.c
@@ -32,6 +32,8 @@ Node * last = NULL;
 void readFile(char * file);
 void findSequence(char * file);
 void Menu(char * file);
+void InsertSequenceDirectly(Paragrafo par);
+void InsertSequence(int sequence, char * tempo, char * text);
 
 int main(int argc,char * argv[]){
 	if(argv[1] == NULL){
@@ -44,53 +46,74 @@ int main(int argc,char * argv[]){
 	return 0;
 }
 
-void findSequence(char * file){
-	//char line = (char*)malloc(sizeof(char));
-	char buff[30];
-	char line[100] = "";
-	FILE * fp;
-	Paragrafo parag;
-        if((fp = fopen(file,"r")) == NULL){                                printf("\nArquivo nao pode ser aberto\n");
-        }else{
-                int ch = getc(fp);
-                if(ch == EOF){                                                     printf("\nArquivo vazio");                                 ungetc(ch,fp);                                     }else{
-			int i = 0;
-			while(1){
-				ungetc(ch,fp);
-				fgets(buff,sizeof(buff),fp);
-				//printf("Gathered: %s",buff);
-				if(buff[0]!='\n'){
+Paragrafo setParagrafo(int numero, char tempo[], char legenda[]){
+	Paragrafo par;
+	par.numero = numero;
+	par.tempo = malloc(sizeof(char));
+	strcpy(par.tempo,tempo);
+	par.legenda = malloc(sizeof(char));
+	strcpy(par.legenda,legenda);
+	return par;
+}
 
-				if(atoi(buff)){
-					parag.numero = atoi(buff);
-					fgets(buff,sizeof(buff),fp);
-					parag.tempo =malloc(sizeof(char));
-					//strcpy(parag.tempo,buff);
-					parag.tempo = buff;
-				char buffer[50];
-				while(1){
-					//fgets(buffer,sizeof(buffer),fp);
-					ch = getc(fp);
-					//printf("Gathered: %s",buffer);
-					//strcat(line,buffer);
-					//if(buffer[0]=='\n')
-					//	break;
-					line[i] = ch;
-					i++;
-					if(ch == '\n'){
-						char fh = getc(fp);
-						if(fh=='\n') break;
-					ungetc(fh,fp);
-					}
-				}
-				parag.legenda = malloc(sizeof(char));
-				strcpy(parag.legenda,line);
-				}
-				//break;	
-				}
+void findSequence(char * file){
+	char buff[30];
+	char buffer[50];
+	char line[500] = "";
+	FILE * fp;
+	int numero = 0;
+	//char * tempo = malloc(sizeof(char));
+	//char * legenda = malloc(sizeof(char));
+	char * tempo;
+	char * legenda;	
+	if((fp = fopen(file,"r")) == NULL){
+		printf("\nArquivo nao pode ser aberto\n");
+	}else{
+		int ch = getc(fp);
+		if(ch == EOF){
+			printf("\nArquivo vazio");
+			ungetc(ch,fp);
+		}else{
+			//int i = 0;
+			ungetc(ch,fp);
+			while(1){			
+				int i = 0;	
 				if(feof(fp)) break;
-			}
-	printf("\n%d\n%s\n%s",parag.numero,parag.tempo,parag.legenda);
+				fgets(buff,sizeof(buff),fp);
+				if(buff[0]!='\n'){
+					if(atoi(buff)){
+						numero = atoi(buff);
+						fgets(buff,sizeof(buff),fp);
+						tempo = buff;						
+						while(1){	
+							char zh = getc(fp);						
+							if(zh == (char)EOF){
+								break;							
+							}else{
+								line[i] = zh;
+								i++;
+								if(zh == '\n'){
+									char fh = getc(fp);
+									if(fh =='\n') break;
+									ungetc(fh,fp);
+								}
+							}							
+						}legenda = line;
+					}
+					//printf("\n%d\n%s\n%s",numero,tempo,legenda);
+					legenda = "";
+					tempo = "";
+					memset(line,0,sizeof(line));
+					memset(buff,0,sizeof(buff));
+					memset(buffer,0,sizeof(buffer));				
+				}//if(feof(fp)) break;
+			}		
+		}
+	}//free(tempo);
+	//free(legenda);
+	fclose(fp);
+}
+
 	//if ch = \n{
 	//	if ch = \n{
 	//		if ch isdigit(){
@@ -98,15 +121,24 @@ void findSequence(char * file){
 	//		}
 	//	}
 	//}
-		}
-	}fclose(fp);
-}
 
 void InsertSequence(int sequence, char * tempo, char * text){
 	Node * aux = (Node*)malloc(sizeof(Node));
 	aux->seq.numero = sequence;
 	aux->seq.tempo = tempo;
 	aux->seq.legenda = text;
+	if(first==NULL){
+		first = aux;
+		last = first;
+	}else{
+		last->next = aux;
+		last = last->next;
+	}last->next = NULL;
+}
+
+void InsertSequenceDirectly(Paragrafo par){
+	Node * aux = (Node*)malloc(sizeof(Node));
+	aux->seq = par;
 	if(first==NULL){
 		first = aux;
 		last = first;
@@ -130,12 +162,13 @@ void readFile(char * file){
 			ungetc(ch,fp);
 			char fh;
 			while(1){
-				//findSequence(fp);
-				//fgets(fh,sizeof(fh),fp);
-				//printf("%s\n", fh);
+				//if(feof(fp)) break;
+				
 				fh = getc(fp);
-				putchar(fh);
-				if(feof(fp)) break;
+				if(fh == (char)EOF){
+					break;
+				}
+				putchar(fh);				
 			}
 		}		
 	}
